24c897be996346e4d026e542263bc15f90ca3e4e
DiyarArstangaliev <d.arstangaliev@gmail.com>
2023-01-15 21:41:49 +0300

    adaptiv

--- src/components/Profile/profile.jsx
+++ src/components/Profile/profile.jsx
@@ -1,42 +1,28 @@
 import React from "react";
-import "./navBar.css";
-import ava from "./foto_1.jpeg";
-//import {NavLink} from "react-router-dom";
+import "./profile.css";
+import "./adaptive.css";
+import {NavLink} from "react-router-dom";
 
-const NavBar = () => {
+const Profile = () => {
   return (
-    <nav className="nav">
-      <div className="nav__ava">
-        <img src={ava} alt="Avatar"/>
-      </div>
-      <div className="nav__items">
-        <div className="nav__item">
-          <span>О нас</span>
-        </div>
-          <div className="nav__item">
-              <span>Букеты</span>
-          </div>
-          <div className="nav__item">
-              <span>Композиции</span>
-          </div>
-          <div className="nav__item">
-              <span>Оформление праздников</span>
-          </div>
-          <div className="nav__item">
-              <span>Как заказать</span>
-          </div>
-          <div className="nav__item">
-              <span>Доставка и оплата</span>
-          </div>
-          <div className="nav__item">
-              <span>Контакты</span>
-          </div>
-          <div className="nav__item">
-              <span>Часто задаваемые вопросы</span>
-          </div>
-      </div>
-    </nav>
+   <div className='app-wrapper-content'>
+       <div className='content-welcome'>
+           Welcome to #1 flower shop
+       </div>
+       <div className='content-title'>
+           Da Flowers
+       </div>
+       <div className='content-subtitle'>
+           Соберём и доставим
+           <br/> в любую точку Москвы и Московской области
+       </div>
+       <div className='content-button'>
+           <NavLink to='/bouquets' className='content-button-text button'>
+               Заказать
+           </NavLink>
+       </div>
+   </div>
   )
 };
 
-export default NavBar;
+export default Profile;
